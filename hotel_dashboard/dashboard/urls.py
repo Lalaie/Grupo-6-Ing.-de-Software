ee1f13cc2f8801f9db78f8f2cd03329bd24a8040
Leonardo-Hurtado <145880163+Leonardo-Hurtado@users.noreply.github.com>
2025-04-28 19:42:57 -0400

    ajustes de navegacion y detalles de imagen

--- hotel_dashboard/dashboard/urls.py
+++ hotel_dashboard/dashboard/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     
     path('', views.hdisponibles, name='hdisponibles'),
     path('habitaciones_disponibles/', views.hdisponibles, name='hdisponibles'), 
-    path('habitacion/reserva/', views.reserva, name='reserva'),
+    path('reserva/', views.reserva, name='reserva'),
     path('habitacion/', views.habitacion, name='habitacion'), 
     path('perfil/', views.perfil, name='perfil'),   
 ]
